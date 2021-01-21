a202e50685b19858dc34d7e83f0ea6ccbeb67067
Shawan Mandal <70664409+imshawan@users.noreply.github.com>
2021-01-22 02:39:38 +0530

    update

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
-  greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
-  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+  name: "Shawan Mandal",
+  greeting: "Hello World! 👋",
+  greetingDescription: "I'm Shawan Mandal and I'm a Student and a Developer!",
+  githubUrl: "https://github.com/imshawan",
+  linkedinUrl: "https://www.linkedin.com/in/shawan-mandal",
+  cvLink: "https://github.com/imshawan/imshawan/blob/main/imshawan-CV.pdf",
 };
